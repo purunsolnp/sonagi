cead402b0c71aa8879216584e634a09bc0069ed4
purunsolnp <purunsolnp@gmail.com>
2025-03-25 21:25:12 +0900

    home 추가

--- app.py
+++ app.py
@@ -10,6 +10,14 @@ from 척도9 import check_exam_availability, exam_categories, alias_to_name
 
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/check", methods=["GET", "POST"])
+def check():
+    return render_template("index.html")
+
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
